59c2e5bd194147b35afb935e38cfe9692c743d1b
lesssweet121 <lesssweet2@gmail.com>
2026-05-22 11:50:26 +0000

    update dbt models and pipeline

--- analysis/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
+++ analysis/taxi_rides_ny/models/staging/stg_yellow_tripdata.sql
@@ -37,6 +37,6 @@ renamed as (
 select * from renamed
 
 -- Sample records for dev environment using deterministic date filter
-{% if target.name == 'dev' %}
-where pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
-{% endif %}
+---{% if target.name == 'dev' %}
+---where pickup_datetime >= '2019-01-01' and pickup_datetime < '2019-02-01'
+---{% endif %}
